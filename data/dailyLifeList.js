3ed4c64518c739d34bfba25f2a7b8a536aafcb47
하진 <khjtwin11@gmail.com>
2026-03-17 00:40:32 +0900

    [Chore]:: 상장, 일상 사진 변경 및 추가

--- data/dailyLifeList.js
+++ data/dailyLifeList.js
@@ -1,10 +1,24 @@
 const dailyLifeList = [
     {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
+    {image: "/daily/IMG_0334.png"},
+    {image: "/daily/IMG_0350.png"},
+    {image: "/daily/IMG_0541.png"},
+    {image: "/daily/IMG_1320.png"},
+    {image: "/daily/IMG_1398.png"},
+    {image: "/daily/IMG_1467.png"},
+    {image: "/daily/IMG_1822.png"},
+    {image: "/daily/IMG_1830.png"},
+    {image: "/daily/IMG_1844.png"},
+    {image: "/daily/IMG_1976.png"},
+    {image: "/daily/IMG_2484.png"},
+    {image: "/daily/IMG_2501.png"},
+    {image: "/daily/IMG_2559.png"},
+    {image: "/daily/IMG_2563.png"},
+    {image: "/daily/IMG_9789.png"},
+    {image: "/daily/IMG_9796.png"},
+    {image: "/daily/IMG_9899.png"},
+    {image: "/daily/IMG_9925.png"},
+
 ]
 
 export default dailyLifeList;
