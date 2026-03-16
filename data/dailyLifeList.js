9007c628f00784553eb85530271074171423fbd1
hjzhhhj <jhj090120@gmail.com>
2026-03-16 21:45:41 +0900

    [Fix] :: 멤버 페이지 사진 수정

--- data/dailyLifeList.js
+++ data/dailyLifeList.js
@@ -1,10 +1,15 @@
 const dailyLifeList = [
-    {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
-    {image: "/daily/IMG_9953.jpg"},
+  { image: "/daily/IMG_0334.png" },
+  { image: "/daily/IMG_0350.png" },
+  { image: "/daily/IMG_0541.png" },
+  { image: "/daily/IMG_1320.png" },
+  { image: "/daily/IMG_1398.png" },
+  { image: "/daily/IMG_1467.png" },
+  { image: "/daily/IMG_1822.png" },
+  { image: "/daily/IMG_1830.png" },
+  { image: "/daily/IMG_1844.png" },
+  { image: "/daily/IMG_1976.png" },
+  { image: "/daily/IMG_9953.jpg" },
 ]
 
 export default dailyLifeList;
