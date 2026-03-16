6f2d7536a5651444ea95af00002c95cac0e90e36
hjzhhhj <jhj090120@gmail.com>
2026-03-16 22:22:15 +0900

    [Fix] :: 멤버 페이지 사진 수정

--- data/dailyLifeList.js
+++ data/dailyLifeList.js
@@ -9,6 +9,16 @@ const dailyLifeList = [
   { image: "/daily/IMG_1830.png" },
   { image: "/daily/IMG_1844.png" },
   { image: "/daily/IMG_1976.png" },
+  { image: "/daily/IMG_1990 2.png" },
+  { image: "/daily/IMG_2484.png" },
+  { image: "/daily/IMG_2501.png" },
+  { image: "/daily/IMG_2559.png" },
+  { image: "/daily/IMG_2563.png" },
+  { image: "/daily/IMG_9787.png" },
+  { image: "/daily/IMG_9789.png" },
+  { image: "/daily/IMG_9796.png" },
+  { image: "/daily/IMG_9899.png" },
+  { image: "/daily/IMG_9925.png" },
   { image: "/daily/IMG_9953.jpg" },
 ]
 
