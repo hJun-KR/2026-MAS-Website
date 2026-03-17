7d60a35e81510e9e57868bb1b289c738f35c962d
hJun-KR <h0ngjunp.park@gmail.com>
2026-03-18 00:28:48 +0900

    [Chore]::vite-plugin-image-optimizer추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      includePublic: true,
+
+      logStats: true,
+      ansiColors: true,
+
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      webp: { quality: 80 },
+
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+          },
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
 })
